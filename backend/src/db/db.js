05c5bf5a2e9f2ab58434f81f3dcdc168247e2d95
Nirakar Bhattarai <01nirakarbhattarai@gmail.com>
2025-05-20 17:44:13 +0545

    First commit

---
+++ backend/src/db/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async()=>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log("MonogoDb connected on : ", conn.connection.host);
+        
+    } catch (error) {
+        console.log("MONGODB connection err: ", error);
+        
+    }
+}
+
---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv"
+import { connectDB } from "./db/db.js";
+
+dotenv.config()
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, (req, res)=>{
+    console.log(`Server listening on port ${PORT}`);
+    connectDB()
+});
