bbff3e898cc15be72593299762df8606b94535e4
Nirakar Bhattarai <01nirakarbhattarai@gmail.com>
2025-06-02 16:55:54 +0545

    ProtectRoute function and cloudinary installed

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken"
+import User from "../models/user.model.js"
+
+export const protectRoute = async(req, res, next) =>{
+    try {
+        const token = req.cookie.jwt
+        if(!token){
+            return res.status(400).json({message: "No token found!!"})
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        if(!decoded){
+            return res.status(400).json({message:"Unauthorized User - Invalid token"})
+        }
+        const user = await User.findById(decoded.userId).select("-password")
+
+        if(!user){
+            return res.status(400).json({message:"User not found !!"})
+        }
+        // when user is found 
+        req.user = user;
+
+        next()
+
+    } catch (error) {
+        console.log("Error in loginCheck: ", error.message);
+        res.status(500).json({message: "Internal Server Error!!"})
+        
+    }
+}
